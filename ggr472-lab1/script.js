e6f80d29078a2b82515fdb1c391227dd4b5ba7f4
gsamue1 <122768087+gsamue1@users.noreply.github.com>
2023-02-02 03:50:00 -0500

    things
    stuff

--- ggr472-lab1/script.js
+++ ggr472-lab1/script.js
@@ -1,37 +1,10 @@
-const options = document.querySelectorAll("label");
-for (let i = 0; i < options.length; i++) {
-  options[i].addEventListener("click", ()=>{
-    for (let j = 0; j < options.length; j++) {
-      if(options[j].classList.contains("selected")){
-        options[j].classList.remove("selected");
-      }
-    }
-
-    options[i].classList.add("selected");
-    for (let k = 0; k < options.length; k++) {
-      options[k].classList.add("selectall");
-    }
-
-    let forVal = options[i].getAttribute("for");
-    let selectInput = document.querySelector("#"+forVal);
-    let getAtt = selectInput.getAttribute("type");
-    if(getAtt == "checkbox"){
-      selectInput.setAttribute("type", "radio");
-    }else if(selectInput.checked == true){
-      options[i].classList.remove("selected");
-      selectInput.setAttribute("type", "checkbox");
-    }
-
-    let array = [];
-    for (let l = 0; l < options.length; l++) {
-      if(options[l].classList.contains("selected")){
-        array.push(l);
-      }
-    }
-    if(array.length == 0){
-      for (let m = 0; m < options.length; m++) {
-        options[m].removeAttribute("class");
-      }
-    }
-  });
+function clicked() {
+    console.log("You Clicked Me!")
 }
+
+//Event listener function 
+var button = document.getElementById("my-btn")
+
+button.addEventListener("click", function() {
+    alert("You Clicked Me!");
+});
